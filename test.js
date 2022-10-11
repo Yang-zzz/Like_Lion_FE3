f3f86990da7b43060fa39fe11400e8290ed3f0ea
Yangjisung <ygji6406@gmail.com>
2022-10-11 21:34:43 +0900

    JS_구조분해_클로저

--- test.js
+++ test.js
@@ -1,34 +1,115 @@
-// const kFoods= ['김치', '비빔밥', '김밥', '떡볶이', '순두부찌개'];
+// let str = "hello world"
+// // 첫번째 인자(start)가, 두번쨰(end) 인자보다 클 경우
+// // substring 의 경우 (5,0) 을 (0,5) 로 변환
+// let a = str.substring(5, 0);
 
-// let info = '꼭 먹어야 할 한국 음식: '
-// for(let i = 0; i < kFoods.length; i++) {
-// 	info += kFoods[i];
-// 	// 조건문은 뒤에 나오는 내용이지만, 깔끔한 결과물 출력을 위해 넣어둡니다.
-// 	if(i < kFoods.length - 1) info += ', ';
-// }
+// // slice 는 빈문자열 반환
+// let b = str.slice(5, 0);
 
-// console.log(info);
+// console.log("substring : " + a); // hello
+// // 첫번째 인자 (start) -> index[0] == 'h' 부터
+// // 두번째 (end) -1 -> index[5-1] == index[4] == o 까지 잘라내어 출력 
 
-// let sum = 0;
-// let i = 0;
+// console.log("slice : " + b);
+// //s
 
-// do {
-// 		i++;
-// 		sum += i;
-// } while (i < 10);
+// let c = 'werr';
+// console.log(isNaN(NaN)); //넘버가 아니다
 
-// console.log(sum);
-// do while 문은 실행문을 작성하지 않아도 되는것?
+// function c() {
+//     for(let i=1; i<10; i++){
+//         if(i < 5) {
+//             console.log(i)
+//             return "return을 일찍 만났다."
+//           } else {
+//             return "i값이, 5초과되면서 return을 만났다."
+//           }
+//            // return문을 사용하지 않는다면 동작문 끝에 return undefined 숨어있음.
+//     }
+//    };
+//    console.log(c());
 
-// timestable:
-// for (let x = 2; x < 10; x++) {
-// 	for (let y = 1; y < 10; y++) {
-// 		if (x === 4) break timestable;
-// 		console.log(`${x} X ${y} = ${x * y}`);
-// 	}
-// }
+//    function a(Q,W,E,R) {
+//     console.log(Q,W,E,R);
+//     console.log(arguments);
+//    }
+   
+//    a("skill1", "skill2", "skill3");
 
-const arr9 = [];
-arr9.length = 3;
-console.log(arr9[0]);
-console.log(Object.getOwnPropertyDescriptors(arr9));
+//    function e() {
+//     for(let i=1; i<5; i++){
+//         console.log(i)
+//     } // return문을 사용하지 않는다면 동작문 끝에 return undefined 숨어있음.
+//    };
+//    console.log(e());
+
+//  const Yang = {
+//     name: 'jisug',
+//     age: 20,
+//     born: 'incheon',
+//   }
+  
+//   const kim = {
+//     name: 'jisug',
+//     age: 20,
+//     born: 'incheon',
+//   }
+  
+//   console.log(Yang===kim);
+
+//   const a = Yang;
+//   const b = kim;
+
+//   console.log(typeof a);
+
+// let x = 3;
+    
+// x == 4 ? console.log('if문으로 실행되었습니다.') : console.log('else문으로 실행되었습니다.');
+
+// let result = x == 4 ? 100 : 200;
+// console.log(result);
+
+
+let score = 71;
+let money = 1000;
+score > 90 ? money += 100000 :
+score > 80 ? money += 10000 :
+score > 70 ? money += 1000 : money = 0
+
+console.log(money);
+
+if(score > 90) {
+    money += 100000
+} else if (score > 80) {
+    money += 10000
+} else if (score > 70) {
+    money += 1000
+} else {
+    money = 0
+}
+console.log(money);
+
+// console.log(`10. ${false == null}<br>`); // 주의 false
+// console.log(`11. ${false == undefined}<br>`); // 주의  false
+
+
+// console.log(`14. ${isNaN(undefined)}<br>`); // true 주의
+// console.log(`15. ${isNaN(null)}<br>`); // false 주의
+// console.log(`16. ${isNaN(NaN)}<br>`); // true 주의
+
+
+// console.log(`17. ${Number.isNaN(undefined)}<br>`); // true 주의   false
+// console.log(`18. ${Number.isNaN(null)}<br>`); // true 주의   false
+// console.log(`19. ${Number.isNaN(NaN)}<br>`); // true 주의   true
+// console.log(`20. ${![]}<br>`); // false 주의   false
+// console.log(`21. ${!{}}<br>`); // false 주의   false
+
+console.log(Boolean());
+// 궁금?!
+// nuber
+
+
+
+let fish = ['정어리', '고등어', '돌고래', '참치', '고래상어', '코끼리']
+console.log(fish.slice(2,3))
+console.log(fish.slice(5,5))
