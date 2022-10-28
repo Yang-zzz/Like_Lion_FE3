57a067e4e646c419b1cd78da9a5bb4ff438ab8dc
Yangjisung <ygji6406@gmail.com>
2022-10-28 23:56:01 +0900

    this_practice

--- test3.js
+++ test3.js
@@ -1,9 +1,20 @@
-class Sausage() {
-  constructor(taste1,taste2) {
-    this.taste1 = taste1
-    this.taste2 = taste2
-  }
-}
+const person = {
+  name: 'Lee',
+  getName() {
+    return this.name;
+  }, // 메서드 내부의 this는 ? == 자신을 호출한 객체를 가르킨다.
+};
+
+console.log(person.getName()); // Lee
+
+console.log(person.getName); // Lee
 
 
+const anotherPerson = {
+  name: 'Kim',
+};
 
+anotherPerson.getName = person.getName;
+console.log(anotherPerson.getName()); // Kim
+
+console.log(person.getName === anotherPerson.getName); // true
--- test_연습용/객체지향_연습.js
+++ test_연습용/객체지향_연습.js
@@ -12,35 +12,43 @@
 
 
 
-// ------메뉴 랜덤선택 생성자함수 만들기 연습------
-// const foodlist = ['짜장면', '짬뽕', '볶음밥', '간짜장'];
+// ------메뉴 랜덤선택 생성자 함수 만들기 연습------
+// const arr = ['짜장면', '짬뽕', '볶음밥', '간짜장'];
 
 // function Pickbot(arr) {
-//   this.arr = arr;
+//   this.empty = arr;
 //   this.select = function() {
-//     console.log(this.arr[Math.floor(Math.random() * this.arr.length)]);
+//     console.log(arr[Math.floor(Math.random() * arr.length)]);
+//     console.log(this.empty[Math.floor(Math.random() * this.empty.length)]);
 //   };
+ select() {
+   return arr[Math.floor(Math.random() * arr.length)];
+ }
 // };
 
 // const pickRobot = new Pickbot(foodlist);
 // pickRobot.select();
-// console.log(pickRobot.select());
 
 
 
 
 // ------ 프로토타입을 활용한 메서드 사용 ------
-// function Pickbot(arr) {
-//   this.arr = arr;
-// }
-// Pickbot.prototype.select = function () {
-//   console.log(this.arr[Math.floor(Math.random() * this.arr.length)])
-// }
+const foodlist = ['짜장면', '짬뽕', '볶음밥', '간짜장'];
 
-// const pickRobot = new Pickbot(['짜장면', '짬뽕', '볶음밥', '간짜장']);
-// console.log(pickRobot.select())
+function Pickbot(arr) {
+  this.list = arr;
+}
 
-// 생성자함수를 통해 만든 인스턴스에서 효율적인 메서드를 사용하기 위해서는 프로토타입을 활용해야 한다.
+Pickbot.prototype.select = function () {
+  return this.list[Math.floor(Math.random() * this.list.length)];
+}
+
+const pickRobot = new Pickbot(foodlist);
+console.log(pickRobot.select());
+console.log(pickRobot);
+
+// 생성자 함수를 통해 만든 인스턴스에서 효율적인 메서드를 사용하기 위해서는 프로토타입을 활용해야 한다.
 // 프로토타입을 사용하기 위해서는
 // 생성자 함수명.prototype.메서드 = function () { 실행 하고자 하는 코드 }
-// 를 작성하는데 this키워드를 사용하여 생성자함수를 참조할 수 있도록 명시하여 사용해야 한다.
+// 를 작성한다.
+// 프로토타입 메서드도 일반 메서드와 마찬가지로 this는 해당 메서드를 호출한 객체에 바인딩 된다. 따라서 프로토타입 메서드 내부에 선언된 this는 자신을 호출한 pickRobot 인스턴스의
